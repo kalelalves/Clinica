033b433907c1ceb9f1344e4c732e9f79e3d1e9ad
Kalel Jamil Alves <73508900+kalelalves@users.noreply.github.com>
2022-08-10 16:50:26 -0300

    Funcionario Criar

--- View/FuncionarioCriar.Designer.cs
+++ View/FuncionarioCriar.Designer.cs
@@ -28,10 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button5 = new System.Windows.Forms.Button();
             this.salvar = new System.Windows.Forms.Button();
             this.titulo_label = new System.Windows.Forms.Label();
-            this.salarioValor = new System.Windows.Forms.TextBox();
-            this.salario_label = new System.Windows.Forms.Label();
+            this.cargoValor = new System.Windows.Forms.TextBox();
+            this.cargo_label = new System.Windows.Forms.Label();
             this.cpfValor = new System.Windows.Forms.TextBox();
             this.cpf_label = new System.Windows.Forms.Label();
             this.cidadeValor = new System.Windows.Forms.TextBox();
@@ -42,167 +43,163 @@
             this.nome_label = new System.Windows.Forms.Label();
             this.codigoValor = new System.Windows.Forms.TextBox();
             this.codigo_label = new System.Windows.Forms.Label();
-            this.cargoValor = new System.Windows.Forms.TextBox();
-            this.cargo_label1 = new System.Windows.Forms.Label();
+            this.salarioValor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1, 1);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 29;
+            this.button5.Text = "Voltar";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // salvar
             // 
-            this.salvar.Location = new System.Drawing.Point(321, 420);
+            this.salvar.Location = new System.Drawing.Point(314, 440);
             this.salvar.Name = "salvar";
             this.salvar.Size = new System.Drawing.Size(75, 23);
-            this.salvar.TabIndex = 27;
+            this.salvar.TabIndex = 28;
             this.salvar.Text = "Salvar";
             this.salvar.UseVisualStyleBackColor = true;
-            this.salvar.Click += new System.EventHandler(this.salvar_Click);
             // 
             // titulo_label
             // 
             this.titulo_label.AutoSize = true;
             this.titulo_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.titulo_label.Location = new System.Drawing.Point(145, 19);
+            this.titulo_label.Location = new System.Drawing.Point(138, 56);
             this.titulo_label.Name = "titulo_label";
             this.titulo_label.Size = new System.Drawing.Size(224, 24);
-            this.titulo_label.TabIndex = 26;
+            this.titulo_label.TabIndex = 27;
             this.titulo_label.Text = "Criar novo Funcionario";
-            this.titulo_label.Click += new System.EventHandler(this.titulo_label_Click);
             // 
-            // salarioValor
+            // cargoValor
             // 
-            this.salarioValor.Location = new System.Drawing.Point(173, 343);
-            this.salarioValor.Name = "salarioValor";
-            this.salarioValor.Size = new System.Drawing.Size(223, 20);
-            this.salarioValor.TabIndex = 25;
-            this.salarioValor.TextChanged += new System.EventHandler(this.doencaValor_TextChanged);
+            this.cargoValor.Location = new System.Drawing.Point(166, 337);
+            this.cargoValor.Name = "cargoValor";
+            this.cargoValor.Size = new System.Drawing.Size(223, 20);
+            this.cargoValor.TabIndex = 26;
             // 
-            // salario_label
+            // cargo_label
             // 
-            this.salario_label.AutoSize = true;
-            this.salario_label.Location = new System.Drawing.Point(94, 350);
-            this.salario_label.Name = "salario_label";
-            this.salario_label.Size = new System.Drawing.Size(39, 13);
-            this.salario_label.TabIndex = 24;
-            this.salario_label.Text = "Salario";
-            this.salario_label.Click += new System.EventHandler(this.doenca_label_Click);
+            this.cargo_label.AutoSize = true;
+            this.cargo_label.Location = new System.Drawing.Point(87, 344);
+            this.cargo_label.Name = "cargo_label";
+            this.cargo_label.Size = new System.Drawing.Size(35, 13);
+            this.cargo_label.TabIndex = 25;
+            this.cargo_label.Text = "Cargo";
             // 
             // cpfValor
             // 
-            this.cpfValor.Location = new System.Drawing.Point(173, 170);
+            this.cpfValor.Location = new System.Drawing.Point(166, 300);
             this.cpfValor.Name = "cpfValor";
             this.cpfValor.Size = new System.Drawing.Size(223, 20);
-            this.cpfValor.TabIndex = 23;
-            this.cpfValor.TextChanged += new System.EventHandler(this.cpfValor_TextChanged);
+            this.cpfValor.TabIndex = 24;
             // 
             // cpf_label
             // 
             this.cpf_label.AutoSize = true;
-            this.cpf_label.Location = new System.Drawing.Point(94, 177);
+            this.cpf_label.Location = new System.Drawing.Point(87, 307);
             this.cpf_label.Name = "cpf_label";
             this.cpf_label.Size = new System.Drawing.Size(30, 13);
-            this.cpf_label.TabIndex = 22;
+            this.cpf_label.TabIndex = 23;
             this.cpf_label.Text = "CPF:";
-            this.cpf_label.Click += new System.EventHandler(this.cpf_label_Click);
             // 
             // cidadeValor
             // 
-            this.cidadeValor.Location = new System.Drawing.Point(173, 258);
+            this.cidadeValor.Location = new System.Drawing.Point(166, 252);
             this.cidadeValor.Name = "cidadeValor";
             this.cidadeValor.Size = new System.Drawing.Size(223, 20);
-            this.cidadeValor.TabIndex = 21;
-            this.cidadeValor.TextChanged += new System.EventHandler(this.cidadeValor_TextChanged);
+            this.cidadeValor.TabIndex = 22;
             // 
             // cidade_label
             // 
             this.cidade_label.AutoSize = true;
-            this.cidade_label.Location = new System.Drawing.Point(94, 265);
+            this.cidade_label.Location = new System.Drawing.Point(87, 259);
             this.cidade_label.Name = "cidade_label";
             this.cidade_label.Size = new System.Drawing.Size(43, 13);
-            this.cidade_label.TabIndex = 20;
+            this.cidade_label.TabIndex = 21;
             this.cidade_label.Text = "Cidade:";
-            this.cidade_label.Click += new System.EventHandler(this.cidade_label_Click);
             // 
             // idadeValor
             // 
-            this.idadeValor.Location = new System.Drawing.Point(173, 216);
+            this.idadeValor.Location = new System.Drawing.Point(166, 210);
             this.idadeValor.Name = "idadeValor";
             this.idadeValor.Size = new System.Drawing.Size(223, 20);
-            this.idadeValor.TabIndex = 19;
-            this.idadeValor.TextChanged += new System.EventHandler(this.idadeValor_TextChanged);
+            this.idadeValor.TabIndex = 20;
             // 
             // idade_label
             // 
             this.idade_label.AutoSize = true;
-            this.idade_label.Location = new System.Drawing.Point(94, 223);
+            this.idade_label.Location = new System.Drawing.Point(87, 217);
             this.idade_label.Name = "idade_label";
             this.idade_label.Size = new System.Drawing.Size(37, 13);
-            this.idade_label.TabIndex = 18;
+            this.idade_label.TabIndex = 19;
             this.idade_label.Text = "Idade:";
-            this.idade_label.Click += new System.EventHandler(this.idade_label_Click);
             // 
             // nomeValor
             // 
-            this.nomeValor.Location = new System.Drawing.Point(173, 129);
+            this.nomeValor.Location = new System.Drawing.Point(166, 166);
             this.nomeValor.Name = "nomeValor";
             this.nomeValor.Size = new System.Drawing.Size(223, 20);
-            this.nomeValor.TabIndex = 17;
-            this.nomeValor.TextChanged += new System.EventHandler(this.nomeValor_TextChanged);
+            this.nomeValor.TabIndex = 18;
             // 
             // nome_label
             // 
             this.nome_label.AutoSize = true;
-            this.nome_label.Location = new System.Drawing.Point(94, 136);
+            this.nome_label.Location = new System.Drawing.Point(87, 173);
             this.nome_label.Name = "nome_label";
             this.nome_label.Size = new System.Drawing.Size(38, 13);
-            this.nome_label.TabIndex = 16;
+            this.nome_label.TabIndex = 17;
             this.nome_label.Text = "Nome:";
-            this.nome_label.Click += new System.EventHandler(this.nome_label_Click);
             // 
             // codigoValor
             // 
-            this.codigoValor.Location = new System.Drawing.Point(173, 84);
+            this.codigoValor.Location = new System.Drawing.Point(166, 121);
             this.codigoValor.Name = "codigoValor";
             this.codigoValor.Size = new System.Drawing.Size(223, 20);
-            this.codigoValor.TabIndex = 15;
-            this.codigoValor.TextChanged += new System.EventHandler(this.codigoValor_TextChanged);
+            this.codigoValor.TabIndex = 16;
             // 
             // codigo_label
             // 
             this.codigo_label.AutoSize = true;
-            this.codigo_label.Location = new System.Drawing.Point(94, 91);
+            this.codigo_label.Location = new System.Drawing.Point(87, 128);
             this.codigo_label.Name = "codigo_label";
             this.codigo_label.Size = new System.Drawing.Size(43, 13);
-            this.codigo_label.TabIndex = 14;
+            this.codigo_label.TabIndex = 15;
             this.codigo_label.Text = "Codigo:";
             this.codigo_label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.codigo_label.Click += new System.EventHandler(this.codigo_label_Click);
             // 
-            // cargoValor
+            // salarioValor
             // 
-            this.cargoValor.Location = new System.Drawing.Point(173, 299);
-            this.cargoValor.Name = "cargoValor";
-            this.cargoValor.Size = new System.Drawing.Size(223, 20);
-            this.cargoValor.TabIndex = 29;
+            this.salarioValor.Location = new System.Drawing.Point(166, 380);
+            this.salarioValor.Name = "salarioValor";
+            this.salarioValor.Size = new System.Drawing.Size(223, 20);
+            this.salarioValor.TabIndex = 31;
             // 
-            // cargo_label1
+            // label1
             // 
-            this.cargo_label1.AutoSize = true;
-            this.cargo_label1.Location = new System.Drawing.Point(94, 306);
-            this.cargo_label1.Name = "cargo_label1";
-            this.cargo_label1.Size = new System.Drawing.Size(35, 13);
-            this.cargo_label1.TabIndex = 28;
-            this.cargo_label1.Text = "Cargo";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 387);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Salario";
             // 
             // FuncionarioCriar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 500);
-            this.Controls.Add(this.cargoValor);
-            this.Controls.Add(this.cargo_label1);
+            this.ClientSize = new System.Drawing.Size(472, 514);
+            this.Controls.Add(this.salarioValor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.salvar);
             this.Controls.Add(this.titulo_label);
-            this.Controls.Add(this.salarioValor);
-            this.Controls.Add(this.salario_label);
+            this.Controls.Add(this.cargoValor);
+            this.Controls.Add(this.cargo_label);
             this.Controls.Add(this.cpfValor);
             this.Controls.Add(this.cpf_label);
             this.Controls.Add(this.cidadeValor);
@@ -222,10 +219,11 @@
 
         #endregion
 
+        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button salvar;
         private System.Windows.Forms.Label titulo_label;
-        private System.Windows.Forms.TextBox salarioValor;
-        private System.Windows.Forms.Label salario_label;
+        private System.Windows.Forms.TextBox cargoValor;
+        private System.Windows.Forms.Label cargo_label;
         private System.Windows.Forms.TextBox cpfValor;
         private System.Windows.Forms.Label cpf_label;
         private System.Windows.Forms.TextBox cidadeValor;
@@ -236,7 +234,7 @@
         private System.Windows.Forms.Label nome_label;
         private System.Windows.Forms.TextBox codigoValor;
         private System.Windows.Forms.Label codigo_label;
-        private System.Windows.Forms.TextBox cargoValor;
-        private System.Windows.Forms.Label cargo_label1;
+        private System.Windows.Forms.TextBox salarioValor;
+        private System.Windows.Forms.Label label1;
     }
 }
--- View/Funcionarios.Designer.cs
+++ View/Funcionarios.Designer.cs
@@ -28,12 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.filtroValor = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listagem = new System.Windows.Forms.DataGridView();
+            this.codp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cpf = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.doenca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.listagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Nome",
+            "CPF"});
+            this.listBox1.Location = new System.Drawing.Point(32, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 17);
+            this.listBox1.TabIndex = 17;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1, 1);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Voltar";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(380, 237);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Novo";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(182, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Deletar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Editar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Nome:";
+            // 
+            // filtroValor
+            // 
+            this.filtroValor.Location = new System.Drawing.Point(158, 30);
+            this.filtroValor.Name = "filtroValor";
+            this.filtroValor.Size = new System.Drawing.Size(100, 20);
+            this.filtroValor.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Filtrar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listagem
+            // 
+            this.listagem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.listagem.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.codp,
+            this.nome,
+            this.idade,
+            this.cidade,
+            this.cpf,
+            this.doenca});
+            this.listagem.Location = new System.Drawing.Point(14, 57);
+            this.listagem.Name = "listagem";
+            this.listagem.Size = new System.Drawing.Size(459, 163);
+            this.listagem.TabIndex = 9;
+            // 
+            // codp
+            // 
+            this.codp.DataPropertyName = "codp";
+            this.codp.HeaderText = "Codigo";
+            this.codp.Name = "codp";
+            this.codp.Visible = false;
+            // 
+            // nome
+            // 
+            this.nome.DataPropertyName = "nome";
+            this.nome.HeaderText = "Nome";
+            this.nome.Name = "nome";
+            // 
+            // idade
+            // 
+            this.idade.DataPropertyName = "idade";
+            this.idade.HeaderText = "Idade";
+            this.idade.Name = "idade";
+            // 
+            // cidade
+            // 
+            this.cidade.DataPropertyName = "cidade";
+            this.cidade.HeaderText = "Cidade";
+            this.cidade.Name = "cidade";
+            // 
+            // cpf
+            // 
+            this.cpf.DataPropertyName = "cpf";
+            this.cpf.HeaderText = "CPF";
+            this.cpf.Name = "cpf";
+            // 
+            // doenca
+            // 
+            this.doenca.DataPropertyName = "doenca";
+            this.doenca.HeaderText = "Doença";
+            this.doenca.Name = "doenca";
+            this.doenca.Visible = false;
+            // 
+            // Funcionarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(508, 293);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filtroValor);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listagem);
+            this.Name = "Funcionarios";
             this.Text = "Funcionarios";
+            ((System.ComponentModel.ISupportInitialize)(this.listagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox filtroValor;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView listagem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cpf;
+        private System.Windows.Forms.DataGridViewTextBoxColumn doenca;
     }
 }
